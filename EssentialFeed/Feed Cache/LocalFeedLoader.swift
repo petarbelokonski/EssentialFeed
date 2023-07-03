2aa2fe289ff16727cb3523da9f0124b11c3f803a
Petar Belokonski <petar.belokonski@tumba.solutions>
2023-07-03 18:49:54 +0300

    Load command deletes 7 days old cache upon rettreival

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -35,11 +35,14 @@ public final class LocalFeedLoader {
         store.retrieve { [unowned self] result in
             switch result {
             case let .failure(error):
-                self.store.deleteCacheFeed { _ in }
+                self.store.deleteCacheFeed { _ in  }
                 completion(.failure(error))
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
-            case .empty, .found:
+            case .found:
+                self.store.deleteCacheFeed { _ in  }
+                completion(.success([]))
+            case .empty:
                 completion(.success([]))
             }
         }
