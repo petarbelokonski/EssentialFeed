b87bf10401516bcc4e43d3b07c770ce1680b34f5
Petar Belokonski <petar.belokonski@tumba.solutions>
2023-07-03 15:05:17 +0300

    Add test for more than than 7 days old cache to test all scenarios

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -44,9 +44,12 @@ public final class LocalFeedLoader {
         }
     }
 
+    private var maxCacheAgeInDays: Int {
+        7
+    }
     private func validate(_ timestamp: Date) -> Bool {
         let calendar = Calendar(identifier: .gregorian)
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }
         return currentDate() < maxCacheAge
