65a149750671733249a436859a9de98253484069
Petar Belokonski <petar.belokonski@tumba.solutions>
2023-09-05 13:21:41 +0300

    Add empty CoreData feed store specs

---
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,83 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Petar Bel on 5.09.23.
+//
+
+import XCTest
+
+final class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
+
+    func test_retrieve_deliversFailureOnRetrievalError() {
+
+    }
+
+    func test_retrieve_hasNoSideEffectsOnFailure() {
+
+    }
+
+    func test_insert_deliversErrorOnInsertionError() {
+
+    }
+
+    func test_insert_hasNoSideEffectsOnInsertionError() {
+
+    }
+
+    func test_delete_deliversErrorOnDeletionError() {
+
+    }
+
+    func test_delete_hasNoSideEffectsOnDeletionError() {
+
+    }
+
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+
+    }
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+
+    }
+
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+
+    }
+
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+
+    }
+
+    func test_insert_deliversNoErrorOnEmptyCache() {
+
+    }
+
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+
+    }
+
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+
+    }
+
+    func test_delete_deliversNoErrorOnEmptyCache() {
+
+    }
+
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+
+    }
+
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+
+    }
+
+    func test_delete_emptiesPreviouslyInsertedCache() {
+
+    }
+
+    func test_storeSideEffects_runSerially() {
+
+    }
+}
