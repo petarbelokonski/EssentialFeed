5f0be22b1d24284aedb8720f68cb65204e1cea24
Petar Belokonski <petar.belokonski@tumba.solutions>
2023-09-13 12:55:56 +0300

    Add Feed storyboard and assets from prototype

--- EssentialFeediOS/Feed UI/Views/FeedImageCell.swift
+++ EssentialFeediOS/Feed UI/Views/FeedImageCell.swift
@@ -13,6 +13,7 @@ public final class FeedImageCell: UITableViewCell {
     public let descriptionLabel = UILabel()
     public let feedImageContainer = UIView()
     public let feedImageView = UIImageView()
+
     private(set) public lazy var feedImageRetryButton: UIButton = {
         let button = UIButton()
         button.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
