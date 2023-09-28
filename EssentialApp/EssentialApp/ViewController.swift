665caf8bf3dac1df4c610f8860fa89bfa61d731b
Petar Belokonski <petar.belokonski@tumba.solutions>
2023-09-28 10:46:46 +0300

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Petar Bel on 28.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
