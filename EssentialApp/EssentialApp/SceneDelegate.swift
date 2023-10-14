a47f85263c1e3d72ac440e6822eeab7e001125d7
Petar Belokonski <petar.belokonski@tumba.solutions>
2023-10-14 16:08:19 +0300

    Move RemoteLoader composition to the Composition Root

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -31,9 +31,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         LocalFeedLoader(store: store, currentDate: Date.init)
     }()
 
-    private lazy var remoteFeedLoader: RemoteFeedLoader = {
+    private lazy var remoteFeedLoader: RemoteLoader = {
         let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-        return RemoteFeedLoader(url: url, client: httpClient)
+        return RemoteLoader(url: url, client: httpClient, mapper: FeedItemsMapper.map)
     }()
 
     convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
