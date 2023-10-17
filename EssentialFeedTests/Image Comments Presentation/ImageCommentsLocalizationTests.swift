6bbb050a1c47ac8d4a1ec812acc4f6715fc5ed33
Petar Belokonski <petar.belokonski@tumba.solutions>
2023-10-17 16:57:56 +0300

    Localized image comments title

---
+++ EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,19 @@
+//
+//  ImageCommentsLocalizationTests.swift
+//  EssentialFeedTests
+//
+//  Created by Petar Bel on 17.10.23.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsLocalizationTests: XCTestCase {
+
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+}
