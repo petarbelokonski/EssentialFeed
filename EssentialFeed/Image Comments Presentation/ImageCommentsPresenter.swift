70a76109607a9659388cc6f88312bf7f32804f7c
Petar Belokonski <petar.belokonski@tumba.solutions>
2023-10-17 18:32:47 +0300

    Map ImageComments into ImageCommentsViewModel

--- EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
+++ EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+public struct ImageCommentsViewModel {
+    public let comments: [ImageCommentViewModel]
+}
+
+public struct ImageCommentViewModel: Equatable {
+    public let message: String
+    public let date: String
+    public let username: String
+
+    public init(message: String, date: String, username: String) {
+        self.message = message
+        self.date = date
+        self.username = username
+    }
+}
+
 public final class ImageCommentsPresenter {
      public static var title: String {
         return NSLocalizedString(
@@ -16,7 +32,15 @@ public final class ImageCommentsPresenter {
             comment: "Title for the image comments view")
     }
 
-    public static func map(_ feed: [FeedImage]) -> FeedViewModel {
-        FeedViewModel(feed: feed)
+    public static func map(_ comments: [ImageComment]) -> ImageCommentsViewModel {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.locale = Locale(identifier: "en-us")
+
+        return ImageCommentsViewModel(comments: comments.map { comment in
+            ImageCommentViewModel(
+                message: comment.message,
+                date: formatter.localizedString(for: comment.createdAt, relativeTo: Date.now),
+                username: comment.username)
+        })
     }
 }
