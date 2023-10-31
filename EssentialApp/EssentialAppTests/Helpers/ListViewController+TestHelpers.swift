a5e4d0f0e12451bcb12f489ef26d2cf721ea077b
Petar Belokonski <petar.belokonski@tumba.solutions>
2023-10-31 17:01:12 +0200

    Renders load more error message

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -139,6 +139,9 @@ extension ListViewController {
         return loadMoreFeedCell()?.isLoading == true
     }
 
+    var loadMoreFeedErrorMessage: String? {
+        return loadMoreFeedCell()?.message
+    }
     private func loadMoreFeedCell() -> LoadMoreCell? {
         cell(row: 0, section: feedLoadMoreSection) as? LoadMoreCell
     }
