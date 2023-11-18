d3e5252ea2a1fb15b9ca7779e3b8730ec32ff800
Petar Belokonski <petar.belokonski@tumba.solutions>
2023-11-18 19:45:40 +0200

    Remove unused async methods from FeedStore protocol

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -18,80 +18,9 @@ public struct CachedFeed {
 }
 
 public protocol FeedStore {
-    typealias DeletionResult = Result<Void, Error>
-    typealias DeletionCompletion = (DeletionResult) -> Void
-
-    typealias InsertionResult = Result<Void, Error>
-    typealias InsertionCompletion = (InsertionResult) -> Void
-
-    typealias RetrievalResult = Result<CachedFeed?, Error>
-    typealias RetrievalCompletion = (RetrievalResult) -> Void
-
-    /// The completion handler can be invoked in any thread.
-    /// Clients are responsible to dispatch to appropriate threads, if needed.
-    @available(*, deprecated)
-    func deleteCacheFeed(completion: @escaping DeletionCompletion)
-
-    /// The completion handler can be invoked in any thread.
-    /// Clients are responsible to dispatch to appropriate threads, if needed.
-    @available(*, deprecated)
-    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
-
-    /// The completion handler can be invoked in any thread.
-    /// Clients are responsible to dispatch to appropriate threads, if needed.
-    @available(*, deprecated)
-    func retrieve(completion: @escaping RetrievalCompletion)
-
     func deleteCacheFeed() throws
 
     func insert(_ feed: [LocalFeedImage], timestamp: Date) throws
 
     func retrieve() throws -> CachedFeed?
-}
-
-public extension FeedStore {
-    func deleteCacheFeed() throws {
-        let dispatchGroup = DispatchGroup()
-        dispatchGroup.enter()
-        var result: DeletionResult!
-        deleteCacheFeed {
-            result = $0
-            dispatchGroup.leave()
-        }
-        dispatchGroup.wait()
-        try result.get()
-    }
-
-    func insert(_ feed: [LocalFeedImage], timestamp: Date) throws {
-        let dispatchGroup = DispatchGroup()
-        dispatchGroup.enter()
-        var result: InsertionResult!
-        insert(feed, timestamp: timestamp) {
-            result = $0
-            dispatchGroup.leave()
-        }
-        dispatchGroup.wait()
-        try result.get()
-    }
-
-    func retrieve() throws -> CachedFeed? {
-        let dispatchGroup = DispatchGroup()
-        dispatchGroup.enter()
-        var result: RetrievalResult!
-        retrieve {
-            result = $0
-            dispatchGroup.leave()
-        }
-        dispatchGroup.wait()
-        return try result.get()
-    }
-
-    func deleteCacheFeed(completion: @escaping DeletionCompletion) {
-    }
-
-    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-    }
-
-    func retrieve(completion: @escaping RetrievalCompletion) {
-    }
 }
--- EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
@@ -114,12 +114,12 @@ extension FeedStoreSpecs where Self: XCTestCase {
         return deletionError
     }
 
-    func expect(_ sut: FeedStore, toRetrieveTwice expectedResult: FeedStore.RetrievalResult, file: StaticString = #file, line: UInt = #line) {
+    func expect(_ sut: FeedStore, toRetrieveTwice expectedResult: Result<CachedFeed?, Error>, file: StaticString = #file, line: UInt = #line) {
         expect(sut, toRetrieve: expectedResult, file: file, line: line)
         expect(sut, toRetrieve: expectedResult, file: file, line: line)
     }
 
-    func expect(_ sut: FeedStore, toRetrieve expectedResult: FeedStore.RetrievalResult, file: StaticString = #file, line: UInt = #line) {
+    func expect(_ sut: FeedStore, toRetrieve expectedResult: Result<CachedFeed?, Error>, file: StaticString = #file, line: UInt = #line) {
         let receivedResult = Result { try sut.retrieve() }
         switch (expectedResult, receivedResult) {
         case (.success(.none), .success(.none)),
